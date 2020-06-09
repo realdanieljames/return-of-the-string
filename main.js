b5e54d4930692bf356ecebe654fe8a79a55002f1
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-09 01:03:27 -0400

    Tue Jun 9 2020, 1:03:13am

--- main.js
+++ main.js
@@ -2,6 +2,61 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(str) {
+  return str + "!";
+}
+const yells = yell('more')
+yells;
+
+
+function getFirstCharacter(firstCharacter) {
+  return firstCharacter[0]
+}
+const one = getFirstCharacter('whenever')
+one;
+
+
+function getLastCharacter(lastCharacter) {
+  return lastCharacter[lastCharacter.length -1]
+}
+const last = getLastCharacter('Daniel')
+last;
+
+function getOneCharacter(aString, aNumber) {
+  return aString[aNumber]
+}
+const character = getOneCharacter('daniel' , 5)
+character;
+
+function getTwoCharacters(phrase, numeral1, numeral2) {
+  return phrase[numeral1] + phrase[numeral2];
+}
+const two_time = getTwoCharacters('roger', 1,2)
+two_time;
+
+function makeCapitalized(term) {
+  return term.toUpperCase();
+}
+const capitalize = makeCapitalized('blue')
+capitalize;
+
+function yellLouder(expression) {
+  return expression.toUpperCase() + '!!!';
+}
+
+// function getInitials(name) {
+//   return name.split(" ")
+//   .map((word) => {
+//     return word[0] + '.';
+//   })
+//   .join('');
+// } 
+
+function getInitials(name) {
+  return name[0];
+
+const initials = getInitials('figure me')
+initials;
 
 
 
